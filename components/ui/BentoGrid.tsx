bec4f13098d291ab7dc2bb0c66f60cef2e9fcdc7
Neel Bhatnagar <bhatnagarneel59@gmail.com>
2026-09-01 16:46:10 +0530

    fixture

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,18 +1,16 @@
-"use client"
+"use client";
 import { useState } from "react";
+import dynamic from "next/dynamic";
 
 import { IoCopyOutline } from "react-icons/io5";
-
-// Also install this npm i --save-dev @types/react-lottie
-import Lottie from "react-lottie";
-
 import { cn } from "@/lib/utils";
-
-
 import { BackgroundGradientAnimation } from "./GradientBg";
 import GridGlobe from "./GridGlobe";
 import animationData from "@/data/confetti.json";
 import MagicButton from "../MagicButton";
+
+// dynamic import, no SSR — avoids "document is not defined" during build
+const Lottie = dynamic(() => import("react-lottie"), { ssr: false });
 
 export const BentoGrid = ({
   className,
